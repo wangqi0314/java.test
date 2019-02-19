a0d2286ae0d7e4a93105ca621cba4d3775bf47ac
qi.wang <qi.wang@riskeys.com>
2019-02-19 14:45:09 +0800

    add base

--- src/main/java/sh/basic_data_type/DateTime.md
+++ src/main/java/sh/basic_data_type/DateTime.md
@@ -18,26 +18,16 @@ Date对象创建以后，可以调用下面的方法。
 
 序号|方法和描述
 ------|------
-1 | boolean after(Date date)
-若当调用此方法的Date对象在指定日期之后返回true,否则返回false。
-2 | boolean before(Date date)
-若当调用此方法的Date对象在指定日期之前返回true,否则返回false。
-3 | Object clone( )
-返回此对象的副本。
-4 | int compareTo(Date date)
-比较当调用此方法的Date对象和指定日期。两者相等时候返回0。调用对象在指定日期之前则返回负数。调用对象在指定日期之后则返回正数。
-5 | int compareTo(Object obj)
-若obj是Date类型则操作等同于compareTo(Date) 。否则它抛出ClassCastException。
-6 | boolean equals(Object date)
-当调用此方法的Date对象和指定日期相等时候返回true,否则返回false。
-7 | long getTime( )
-返回自 1970 年 1 月 1 日 00:00:00 GMT 以来此 Date 对象表示的毫秒数。
-8 | int hashCode( )
- 返回此对象的哈希码值。
-9 | void setTime(long time)
-用自1970年1月1日00:00:00 GMT以后time毫秒数设置时间和日期。
-10 | String toString( )
-转换Date对象为String表示形式，并返回该字符串。
+1 | boolean after(Date date) 若当调用此方法的Date对象在指定日期之后返回true,否则返回false。
+2 | boolean before(Date date) 若当调用此方法的Date对象在指定日期之前返回true,否则返回false。
+3 | Object clone( ) 返回此对象的副本。
+4 | int compareTo(Date date) 比较当调用此方法的Date对象和指定日期。两者相等时候返回0。调用对象在指定日期之前则返回负数。调用对象在指定日期之后则返回正数。
+5 | int compareTo(Object obj) 若obj是Date类型则操作等同于compareTo(Date) 。否则它抛出ClassCastException。
+6 | boolean equals(Object date) 当调用此方法的Date对象和指定日期相等时候返回true,否则返回false。
+7 | long getTime( ) 返回自 1970 年 1 月 1 日 00:00:00 GMT 以来此 Date 对象表示的毫秒数。
+8 | int hashCode( ) 返回此对象的哈希码值。
+9 | void setTime(long time) 用自1970年1月1日00:00:00 GMT以后time毫秒数设置时间和日期。
+10 | String toString( ) 转换Date对象为String表示形式，并返回该字符串。
 
 ### 日期比较
 
---
+++ src/main/java/sh/basic_data_type/RregularExpressionsTest.java
@@ -0,0 +1,92 @@
+package sh.basic_data_type;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+public class RregularExpressionsTest {
+
+	@Test
+	public void Test() {
+
+		// 按指定模式在字符串查找
+		String line = "This order was placed for QT3000! OK?";
+		String pattern = "(.*)(\\d+)(.*)";
+
+		// 创建 Pattern 对象
+		Pattern r = Pattern.compile(pattern);
+
+		// 现在创建 matcher 对象
+		Matcher m = r.matcher(line);
+		if (m.find()) {
+			System.out.println("Found value: " + m.group(0));
+			System.out.println("Found value: " + m.group(1));
+			System.out.println("Found value: " + m.group(2));
+		} else {
+			System.out.println("NO MATCH");
+		}
+	}
+
+	@Test
+	public void Test2() {
+		String REGEX = "\\bcat\\b";
+		String INPUT = "cat cat cat cattie cat";
+
+		Pattern p = Pattern.compile(REGEX);
+		Matcher m = p.matcher(INPUT); // 获取 matcher 对象
+		int count = 0;
+		while (m.find()) {
+			count++;
+			System.out.println("Match number " + count);
+			System.out.println("start(): " + m.start());
+			System.out.println("end(): " + m.end());
+		}
+	}
+
+	@Test
+	public void Test3() {
+		String REGEX = "foo";
+		String INPUT = "fooooooooooooooooo";
+
+		Pattern pattern = Pattern.compile(REGEX);
+		Matcher matcher = pattern.matcher(INPUT);
+
+		System.out.println("Current REGEX is: " + REGEX);
+		System.out.println("Current INPUT is: " + INPUT);
+
+		System.out.println("lookingAt(): " + matcher.lookingAt());
+		System.out.println("matches(): " + matcher.matches());
+	}
+
+	@Test
+	public void Test4() {
+		String REGEX = "dog";
+		String INPUT = "The dog says meow. " + "All dogs say meow.";
+		String REPLACE = "cat";
+
+		Pattern p = Pattern.compile(REGEX);
+		// get a matcher object
+		Matcher m = p.matcher(INPUT);
+		INPUT = m.replaceAll(REPLACE);
+		System.out.println(INPUT);
+	}
+
+	@Test
+	public void Test5() {
+		String REGEX = "a*b";
+		String INPUT = "aabfooaabfooabfoob";
+		String REPLACE = "-";
+
+		Pattern p = Pattern.compile(REGEX);
+		// 获取 matcher 对象
+		Matcher m = p.matcher(INPUT);
+		StringBuffer sb = new StringBuffer();
+		while (m.find()) {
+			m.appendReplacement(sb, REPLACE);
+		}
+		m.appendTail(sb);
+		System.out.println(sb.toString());
+	}
+
+}
