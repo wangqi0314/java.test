f9f322c4442ad66a9bd7e4c94bebacaa3f48d6ce
qi.wang <qi.wang@riskeys.com>
2019-04-10 16:10:38 +0800

    add

--- src/main/java/sh/generic/Util.java
+++ src/main/java/sh/generic/Util.java
@@ -1,9 +1,18 @@
 package sh.generic;
 
 public class Util {
+
 	public static <K, V> boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
 
 		return p1.getKey().equals(p2.getKey()) && p1.getValue().equals(p2.getValue());
+	}
+
+	public static <T extends Comparable<T>> int countGreaterThan(T[] anArray, T elem) {
+		int count = 0;
+		for (T e : anArray)
+			if (e.compareTo(elem) > 0)
+				++count;
+		return count;
 	}
 
 	public static void main(String... args) {
